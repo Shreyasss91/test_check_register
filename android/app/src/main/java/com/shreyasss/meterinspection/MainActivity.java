6db6e95b10260785f1f01cd9f3cf87a83e1318df
Shreyasss91 <130837478+Shreyasss91@users.noreply.github.com>
2026-09-08 12:55:48 +0530

    fix(android): remove unnecessary AppCompat dependency

--- android/app/src/main/java/com/shreyasss/meterinspection/MainActivity.java
+++ android/app/src/main/java/com/shreyasss/meterinspection/MainActivity.java
@@ -1,13 +1,13 @@
 package com.shreyasss.meterinspection;
 
+import android.app.Activity;
 import android.net.Uri;
 import android.os.Bundle;
 import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.browser.customtabs.CustomTabsIntent;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
